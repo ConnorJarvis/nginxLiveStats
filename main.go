403fcdcab4a2d47e89332bd3c64ac01cf0636b2f
ConnorJarvis <connorpjarvis@gmail.com>
2019-03-05 17:46:37 -0500

    Live users are now based off of the ID allowing multiple streams to be counted

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"net/http"
 	"os"
 	"strconv"
@@ -21,13 +20,12 @@ func main() {
 var lastRun int64
 var userCount int
 
-func countLiveUsers() int {
-	fmt.Println("Disk access")
+func countLiveUsers(id string) int {
 	file, _ := os.Open("/var/log/nginx/access.log")
 	scanner := bufio.NewScanner(file)
 	var ips []string
 	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), ".m3u8") {
+		if strings.Contains(scanner.Text(), id+".m3u8") {
 			layout := "02/Jan/2006:15:04:05"
 			t, _ := time.Parse(layout, strings.Split(scanner.Text(), " ")[3][1:len(strings.Split(scanner.Text(), " ")[3])])
 			if (time.Now().Unix() - t.Unix()) < 300 {
@@ -43,13 +41,19 @@ func countLiveUsers() int {
 }
 
 func streamStats(w http.ResponseWriter, r *http.Request) {
-	if (time.Now().Unix() - lastRun) > 10 {
-		lastRun = time.Now().Unix()
-		userCount = countLiveUsers()
+	id := r.URL.Query().Get("id")
+	if id != "" {
+		if (time.Now().Unix() - lastRun) > 10 {
+			lastRun = time.Now().Unix()
+			userCount = countLiveUsers(id)
+		}
+
+		message := strconv.Itoa(userCount)
+		w.Write([]byte(message))
+	} else {
+		w.Write([]byte("No ID specified"))
 	}
 
-	message := strconv.Itoa(userCount)
-	w.Write([]byte(message))
 }
 
 func stringInSlice(a string, list []string) bool {
